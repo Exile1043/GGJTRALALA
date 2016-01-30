96560ba8814ef6899725ec59aa77ef9ef8a36a30
Quinten Alflen <Exile1043@gmail.com>
2016-01-30 11:21:05 +0100

    Changed RandomGem.cs to allow gems to spawn in outer layers. Tested, ready to merge to Master.

--- GGJTRALALA2/Assets/Scripts/RandomGem.cs
+++ GGJTRALALA2/Assets/Scripts/RandomGem.cs
@@ -29,10 +29,10 @@ namespace Completed
             gridGemPositions.Clear();
 
             //Loop through x axis (columns).
-            for (int x = 1; x < columns - 1; x++)
+            for (int x = 1; x < columns + 1; x++)
             {
                 //Within each column, loop through y axis (rows).
-                for (int y = 1; y < rows - 1; y++)
+                for (int y = 1; y < rows + 1; y++)
                 {
                     //At each index add a new Vector3 to our list with the x and y coordinates of that position.
                     gridPositions.Add(new Vector3(x, y, 0f));
@@ -49,10 +49,10 @@ namespace Completed
             boardHolder = new GameObject("Board").transform;
 
             //Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
-            for (int x = -1; x < columns + 1; x++)
+            for (int x = 1; x < columns + 1; x++)
             {
                 //Loop along y axis, starting from -1 to place floor or outerwall tiles.
-                for (int y = -1; y < rows + 1; y++)
+                for (int y = 1; y < rows + 1; y++)
                 {
                     //Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
                     GameObject toInstantiate = floor;
