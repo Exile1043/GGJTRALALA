afe9747fc53ec6a456d7d2b19a2529ea78e98e13
Jimmy Vermeulen <vermeulen.jimmy@gmail.com>
2016-01-30 15:35:15 +0100

    Fixed movement with gameboard

--- GGJTRALALA2/Assets/Script/Player2MovementGrid.cs
+++ GGJTRALALA2/Assets/Script/Player2MovementGrid.cs
@@ -5,35 +5,53 @@ public class Player2MovementGrid : MonoBehaviour {
 
     Vector3 pos;
     public float speed;
+    CreateGameboard gameboard;
 
     // Use this for initialization
     void Start()
     {
+        gameboard = GameObject.Find("Gameboard").GetComponent<CreateGameboard>();
         pos = transform.position;
+        //Debug.Log(gameboard.GetTilePosition(new Vector2(5,5))[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position == pos)
+        Debug.Log(gameboard.tileSize);
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position == pos && gameboard.GetTileGameObject(transform.position + new Vector3(-gameboard.tileSize, 0)) != null) 
         {
-            pos += Vector3.left;
-            transform.localEulerAngles = new Vector3(0, 0, 180);
+            if (gameboard.GetTileGameObject((Vector2)transform.position + new Vector2(-gameboard.tileSize, 0)).tag == "Tile")
+            {
+                pos += new Vector3(-gameboard.tileSize, 0);
+                transform.localEulerAngles = new Vector3(0, 0, 180);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position == pos)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position == pos && gameboard.GetTileGameObject(transform.position + new Vector3(gameboard.tileSize, 0)) != null)
         {
-            pos += Vector3.right;
-            transform.localEulerAngles = new Vector3(0, 0, 0);
+
+            Debug.Log("right");
+            if (gameboard.GetTileGameObject((Vector2)transform.position + new Vector2(gameboard.tileSize, 0)).tag == "Tile")
+            {
+                pos += new Vector3(gameboard.tileSize, 0);
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position == pos)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position == pos && gameboard.GetTileGameObject(transform.position + new Vector3(0, gameboard.tileSize)) != null)
         {
-            pos += Vector3.up;
-            transform.localEulerAngles = new Vector3(0, 0, 90);
+            if (gameboard.GetTileGameObject((Vector2)transform.position + new Vector2(0, gameboard.tileSize)).tag == "Tile")
+            {
+                pos += new Vector3(0, gameboard.tileSize);
+                transform.localEulerAngles = new Vector3(0, 0, 90);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position == pos)
+        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position == pos && gameboard.GetTileGameObject(transform.position + new Vector3(0, -gameboard.tileSize)) != null)
         {
-            pos += Vector3.down;
-            transform.localEulerAngles = new Vector3(0, 0, 270);
+            if (gameboard.GetTileGameObject((Vector2)transform.position + new Vector2(0, -gameboard.tileSize)).tag == "Tile")
+            {
+                pos += new Vector3(0, -gameboard.tileSize);
+                transform.localEulerAngles = new Vector3(0, 0, 270);
+            }
         }
 
         transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
