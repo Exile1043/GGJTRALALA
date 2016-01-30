bcbfda4d1c89864bf9a41ab46c169a10563de993
Tim <timbrans01@hotmail.com>
2016-01-30 12:32:19 +0100

    Added 2nd player & sprite turn

--- GGJTRALALA2/Assets/Script/Player2MovementGrid.cs
+++ GGJTRALALA2/Assets/Script/Player2MovementGrid.cs
@@ -1,40 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovementGrid : MonoBehaviour {
+public class Player2MovementGrid : MonoBehaviour {
 
     Vector3 pos;
     public float speed;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         pos = transform.position;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.GetKeyDown(KeyCode.A) && transform.position == pos)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position == pos)
         {
             pos += Vector3.left;
             transform.localEulerAngles = new Vector3(0, 0, 180);
         }
-        if(Input.GetKeyDown(KeyCode.D) && transform.position == pos)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position == pos)
         {
             pos += Vector3.right;
             transform.localEulerAngles = new Vector3(0, 0, 0);
         }
-        if(Input.GetKeyDown(KeyCode.W) && transform.position == pos)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position == pos)
         {
             pos += Vector3.up;
             transform.localEulerAngles = new Vector3(0, 0, 90);
         }
-        if(Input.GetKeyDown(KeyCode.S) && transform.position == pos)
+        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position == pos)
         {
             pos += Vector3.down;
             transform.localEulerAngles = new Vector3(0, 0, 270);
         }
 
         transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
-        
+
     }
 }
